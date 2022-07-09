026927efc7f6d076d183c3d6db3934228c9a0be6
SDTakeuchi <kuranku191952996@gmail.com>
2022-07-09 22:41:21 +0900

    sliceutils created

---
+++ slice/slice_utils.go
@@ -0,0 +1,42 @@
+package sliceutils
+
+func IndexOf[T comparable](s []T, elem T) int {
+	/*
+	returns index of given elem in s
+	if elem was not found in s slice, returns -1
+	*/
+	for i, e := range s {
+		if e == elem {
+			return i
+		}
+	}
+	return -1
+}
+
+func Push[T interface{}](s []T, elem T) []T {
+	return append(s, elem)
+}
+
+func Pop[T interface{}](s []T) []T {
+	return s[:len(s)-1]
+}
+
+func Unshift[T interface{}](s []T, elem T) []T {
+	return append([]T{elem}, s...)
+}
+
+func Shift[T interface{}](s []T) []T {
+	return s[1:]
+}
+
+func Insert[T interface{}](s []T, v T, index int) []T {
+	var tempVal T
+	s = append(s, tempVal)
+	s = append(s[:index+1], s[index:len(s)-1]...)
+	s[index] = v
+	return s
+}
+
+func Remove[T interface{}](s []T, index int) []T {
+	return append(s[:index], s[index+1:]...)
+}
